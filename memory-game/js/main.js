5bc1fffaf36ab49f66e4b50ca45eef0ff89bcb98
Jeremy Hilado <Jeremy@jeremys-mbp-2.attlocal.net>
2020-02-18 01:38:54 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,18 +1,23 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let card0ne = cards[2];
-cardsInPlay.push(card0ne);
-let cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-		alert("Sorry, try again.")
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
 	}
-}
+};
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+flipCard(2);
+
+
